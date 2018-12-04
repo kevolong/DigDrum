481a4d66d12a8a204a100aaabceea0700cc95c1c
Kevin Andrew Long <kevolong@gmail.com>
2018-12-04 10:44:46 -0600

    Updates

---
+++ precache-manifest.7b2df314863b5cc78295c9c864bc4320.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f92dd7df816d63f3aad0",
+    "url": "/DigDrum/static/js/runtime~main.f92dd7df.js"
+  },
+  {
+    "revision": "62f2f7299292943a38db",
+    "url": "/DigDrum/static/js/main.62f2f729.chunk.js"
+  },
+  {
+    "revision": "b1bc0e55a72bb937fda9",
+    "url": "/DigDrum/static/js/1.b1bc0e55.chunk.js"
+  },
+  {
+    "revision": "a23c26c08ec220592d8a642d2e6eb393",
+    "url": "/DigDrum/index.html"
+  }
+];
